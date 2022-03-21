dae623058092a2922281b1d8cd6b727fe7c67509
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-21 06:20:18 -0300

    lat

--- CodecademyCSharp/CodecademyCSharp/Program.cs
+++ CodecademyCSharp/CodecademyCSharp/Program.cs
@@ -6,7 +6,12 @@ namespace CodecademyCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(".-.");
+            Console.WriteLine(" .-. ");
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| O |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("Gasparzinho, Buh.");
         }
     }
 }
