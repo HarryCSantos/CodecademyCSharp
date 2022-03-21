513637a75b2ff485750e05dea84007a3b4fec3cf
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-21 06:49:54 -0300

    alt

--- CodecademyCSharp/CodecademyCSharp/Program.cs
+++ CodecademyCSharp/CodecademyCSharp/Program.cs
@@ -12,6 +12,15 @@ namespace CodecademyCSharp
             Console.WriteLine("|   |");
             Console.WriteLine("'~~~'");
             Console.WriteLine("Gasparzinho, Buh.");
+
+            //The backslash \ is an escape character in C#, so
+            // \" will show up as one quote int the output (")
+            // and \\ will show up as one backslashin the output (\).
+            // * Pay atention SPACES MATTER! " (" is not the same as "(".
+
+
+
+
         }
     }
 }
